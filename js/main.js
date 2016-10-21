32cc3cc764bbfdf43971b23130edb7bab16b5687
kbrawley95 <kieran.brawley95@gmail.com>
2016-10-22 00:23:12 +0100

    Results (OnLoad)
    Results are displayed when html is interpreted.

--- js/main.js
+++ js/main.js
@@ -3,11 +3,13 @@ console.log(previousContent);
 
 $('#search').keyup(function(){
 
-  var searchTerm=$('#search').val();
+  var searchTerm=$('#search').val();    //Assign value of search field
   var myExp = new RegExp(searchTerm, "i");
 
+/*JQuery Process that compares the searchTerm with the data present in the .json array. If a match is deteched the content of the webpage is updated appropriately*/
   $.getJSON('./data/museums.json', function(data)
   {
+
     var output='<ul class="searchresults">';
     $.each(data, function(key, val){
       if(val.name.search(myExp)!=-1 || val.address.search(myExp)!=-1){
@@ -19,6 +21,21 @@ $('#search').keyup(function(){
     });
     output+='</ul>';
     $('.carousel-caption').html(output);
-  }); //getJSON
+  });
 
 })
+
+function loadResults(){
+  $.getJSON('./data/museums.json', function(data)
+  {
+    var output='<ul class="searchresults">';
+    $.each(data, function(key, val){
+        output += '<li>';
+        output += '<a href="'+val.website+'">' + val.name +'</a>';
+        output += '<p>'+val.address+'</p>';
+        output += '</li>';
+    });
+    output+='</ul>';
+    $('.carousel-caption').html(output);
+  });
+}
